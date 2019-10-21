ee913499cac75b0bf16b7459d878472a5935cebd
Sydney Blom <sydneyblom@#gmail.com>
2019-10-21 17:07:38 -0500

    Register page done

---
+++ onelineaday-client/src/Components/Register.js
@@ -0,0 +1,58 @@
+
+import React from "react";
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+import {Form, Button} from "semantic-ui-react";
+
+
+
+
+
+class RegForm extends React.Component {
+    state = {
+      credentials: {
+        email: '',
+        password: '',
+        confirmpassword: ''
+      }
+    };
+  
+    handleChange = (e) => {
+        this.setState({
+          credentials: {
+            ...this.state.credentials,
+            [e.target.name]: e.target.value
+          }
+        });
+      };
+    
+
+      register = (e) => {
+        e.preventDefault();
+        axiosWithAuth()
+          .post('/api/auth/register', this.state.credentials)
+          .then(res => {
+            localStorage.setItem('token', res.data.token);
+            this.props.history.push('/Dashboard');
+          })
+          .catch(error => console.log(error));
+      };
+    
+
+  render() {
+    return (
+        <div>
+        <h1>Create a Profile </h1>
+        <Form onSubmit={this.register}>
+         <input type="email" name="email" placeholder="Email" />
+         <input type="password" name="passwordcreate" placeholder="Password" />
+         <input type="password" name="confirmpassword" placeholder="Confirm Password" />
+        <Button>Submit</Button>
+        </Form>
+        </div>
+        
+        );
+  }
+};
+
+
+export default RegForm;
