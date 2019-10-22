8260bfc5990c378df2713d9a6b6e08b37e21481d
Sydney Blom <sydneyblom@#gmail.com>
2019-10-22 16:30:47 -0500

    need a few bug fixes within addentry but works

---
+++ onelineaday-client/src/Components/addEntry.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect }from 'react';
+import axiosWithAuth from "../utils/axiosWithAuth";
+import { Form, TextArea, Button } from 'semantic-ui-react'
+
+
+function AddEntry (props) {
+const [entry, setEntry] = useState([]);
+
+useEffect(() => {
+
+}, )
+
+
+
+function handleChanges(e) {
+    setEntry({...entry, [e.target.name]: e.target.value})
+  };
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    const id = localStorage.getItem('user_id');
+    
+    const finalEntry = new Object;
+
+    finalEntry.title = ' ';
+    finalEntry.text_entry = entry.entry;
+    
+       axiosWithAuth()
+       .post('/api/journal/users/' + id + '/posts', finalEntry)
+       .then(res => {
+           setEntry(res.data.posts);
+           props.loadPosts();
+         })
+         .catch(err => {
+           console.log(err)
+       });
+  }
+
+
+  return (
+    <div >
+      <h2 className='SubTitle'>Add An Entry</h2>
+      <Form onSubmit={handleSubmit}>
+        <TextArea style={{ maxWidth: 400 }}
+          type="text"
+          name="entry"
+          placeholder ="Enter your daily lines here"
+          onChange={handleChanges}
+        />
+        <Button size='medium' color='teal'>Add New Entry</Button>   
+
+      </Form>
+    </div>
+  );
+};
+
+export default AddEntry;
